0844bb3dbd0378b2bd03cdaf6805010e1bafbcbc
Agatha1999 <122836253+Agatha1999@users.noreply.github.com>
2025-04-15 17:12:51 -0300

    Delete site compras directory

--- site compras/Páginas/Contato/enviar.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contatoForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
-    document.getElementById('mensagemSucesso').style.display = 'block'; // Exibe a mensagem de sucesso
-    this.reset(); // Reseta o formulário
-});
